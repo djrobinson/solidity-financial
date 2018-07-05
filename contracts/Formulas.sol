6b9223cf8abbc301c310ba1929107a41ecbd99fd
Danny Robinson <daniel@djrobinson.me>
2018-07-05 17:11:33 -0600

    Major loan formulas working now

--- contracts/Formulas.sol
+++ contracts/Formulas.sol
@@ -2,6 +2,11 @@ pragma solidity ^0.4.17;
 
 contract FinancialFormulas {
 
+    uint public constant d = 1000000000;
+    uint public payment = 0;
+    uint public spcaFactor = 0;
+    uint public requestedAmount = 0;
+
     // TODO: PV(rate, nper, [fv], [type])
     function pv() public {
 
@@ -37,42 +42,48 @@ contract FinancialFormulas {
 
     }
 
-    // PMT(rate, nper, pv, [fv], [type])
-    function calculatePayment(uint requestedRate, uint lengthInPeriods, uint requestedAmount) private returns (uint) {
-        uint numerator = fv(requestedRate, lengthInPeriods, requestedAmount, 10) / requestedRate * 100000000;
-        uint denominator = fv(requestedRate, lengthInPeriods, 100000000, 10) - 100000000;
-        uint payment = numerator / denominator;
-        return payment;
-    }
-
-    // TODO: PPMT(rate, per, nper, pv, [fv], [type])
-    function ppmt() public {
-
-    }
-
-    // IPMT(rate, per, nper, pv, [fv], [type])
-    // Not right yet
-    function ipmt(uint principalBalance, uint requestedRate) public returns (uint) {
-        uint interestPayment = principalBalance / requestedRate;
-        return interestPayment;
-    }
-
-    // FV(rate, nper, pmt, [pv], [type])
-    // rate: interest rate/period
-    // nper: number of periods
-    // pmt: payment/period
-    // pv: present value
-    // type: start or end of period
-    function fv( uint rateBps, uint n, uint presentValue, uint precision) public returns (uint) {
-
+    function calculateSpcaFactor( uint rateBps, uint n, uint precision) public returns (uint) {
       uint s = 0;
       uint N = 1;
       uint B = 1;
       for (uint i = 0; i < precision; ++i){
-        s += presentValue * N / B / ((1 / rateBps * 100 / 10000)**i);
+        uint partOne = d * N / B ;
+        s += partOne *  rateBps**i / 10000**i;
         N  = N * (n-i);
         B  = B * (i+1);
       }
+      spcaFactor = s;
       return s;
+    }
+
+    function calculatePayment(uint requestedRate, uint lengthInPeriods, uint _requestedAmount, uint futureValue) public returns (uint) {
+        uint numerator = calculateSpcaFactor(requestedRate, lengthInPeriods, 10) * _requestedAmount * requestedRate / 10000;
+        uint denominator = calculateSpcaFactor(requestedRate, lengthInPeriods, 10) - d;
+        uint calculatedPayment = numerator / denominator;
+        payment = calculatedPayment;
+        requestedAmount = _requestedAmount;
+        return calculatedPayment;
+    }
+
+    function balance( uint rateBps, uint period, uint lengthInPeriods, uint loanAmount ) public returns (uint) {
+        uint newBalance = (( loanAmount * calculateSpcaFactor(rateBps, period, 20) ) - ( ( payment * 10000 / rateBps )  * ( calculateSpcaFactor(rateBps, period, 20)  - d ))) / d;
+        return newBalance;
+    }
+
+    //   FV(interest_rate, number_payments, payment, PV, Type)
+    function fv(uint rateBps, uint lengthInPeriods, uint payment, uint presentValue) public returns (uint) {
+      uint futureValue = calculateSpcaFactor(rateBps, lengthInPeriods, 20) * presentValue;
+      return futureValue;
+    }
+
+    function ppmt(uint rateBps, uint period, uint lengthInPeriods, uint presentValue, uint futureValue) public returns (uint) {
+        uint principalPayment = payment - ipmt(rateBps, period, lengthInPeriods, presentValue, futureValue);
+        return principalPayment;
+    }
+
+    // IPMT(interest_rate, period, number_payments, PV, FV, Type)
+    function ipmt(uint rateBps, uint period, uint lengthInPeriods, uint presentValue, uint futureValue) public returns (uint) {
+      uint interestPayment = payment + calculateSpcaFactor(rateBps, period - 1, 20)  / d * (balance(rateBps, period - 1, lengthInPeriods, presentValue) * rateBps / 10000 - payment);
+      return interestPayment;
     }
 }
